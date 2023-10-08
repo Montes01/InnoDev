c9317d589bb622bff68b0094971dece6ab45b9a3
sebastiangallego0317 <samuelmontes1105@gmail.com>
2023-10-07 20:27:22 -0500

    added some buttons and fixed responsive problems in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,11 +7,11 @@ export const Header = ({ children }: Props) => {
   const [isFixed, setIsFixed] = useState(false);
   const StickyButtons = document.getElementById("StickyButtons");
   const newStyle =
-    " bg-primary fixed pt-[1.3rem] rounded-b-xl border border-[#aaa] pb-[1.3rem] top-0 z-50 border-x-2";
+    " bg-primary fixed pt-[1.3rem] w-full rounded-b-xl border border-[#aaa] pb-[1.3rem] top-0 z-50 border-x-2";
   if (StickyButtons) {
-    if (isFixed) StickyButtons.className += newStyle;
+    if (isFixed) StickyButtons.className = StickyButtons.className.replace("w-3/6", newStyle);
     else
-      StickyButtons.className = StickyButtons.className.replace(newStyle, "");
+      StickyButtons.className = StickyButtons.className.replace(newStyle, "w-3/6");
   }
 
   document.addEventListener("scroll", () => {
@@ -19,7 +19,8 @@ export const Header = ({ children }: Props) => {
     else if (window.scrollY < 45) setIsFixed(false);
   });
   return (
-    <header className="max-w-[1000px] border-x border-[#aaa] z-20 w-full relative flex flex-col top-0 items-center justify-center gap-3 h-40 p-5 bg-primary">
+    <header 
+      className="1500px:border-x 2000px:border-[#ccc] 1500px:flex-row 1500px:justify-center z-20 w-full relative flex flex-col top-0 items-center justify-center gap-3 h-40 1500px:h-32 p-5 bg-primary">
       {children}
     </header>
   );
