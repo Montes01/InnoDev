5418a349f33fd9394351fc980995bff070839e36
sebastiangallego0317 <samuelmontes1105@gmail.com>
2023-10-10 20:52:25 -0500

    Header into Layout, make hamburguer, !Not working¡

---
+++ src/Sections/General/Hamburguer.tsx
@@ -0,0 +1,25 @@
+
+
+export const Hamburguer = () => {
+
+
+  const handleClick = () => {
+    
+  }
+
+  return (
+
+    <section class="mobile:flex fixed z-50 bg-primary hidden w-full p-5">
+      <button>
+        <svg width="24" height="24" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M5.75 5.25h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 0 1 0-1.5zm0 6h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 1 1 0-1.5zm0 6h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 1 1 0-1.5z"
+          ></path>
+        </svg>
+      </button>
+    </section>
+  )
+}
+
--- src/Sections/General/Header/Header.tsx
+++ src/Sections/General/Header/Header.tsx
@@ -12,13 +12,6 @@ export const Header = ({ children }: Props) => {
   });
   return (
     <header className="mobile:flex-col mobile:h-screen mobile:fixed mobile:p-5 1500px:border-x 2000px:border-[#ccc] 1500px:flex-row 1500px:justify-center z-20 w-full relative flex flex-col top-0 items-center justify-center gap-3 h-40 1500px:h-32 p-5 bg-primary  mobile:translate-x-[-100%]">
-      <svg width="24" height="24" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          fill-rule="evenodd"
-          d="M5.75 5.25h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 0 1 0-1.5zm0 6h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 1 1 0-1.5zm0 6h12.5a.75.75 0 1 1 0 1.5H5.75a.75.75 0 1 1 0-1.5z"
-        ></path>
-      </svg>
       {children}
     </header>
   );
